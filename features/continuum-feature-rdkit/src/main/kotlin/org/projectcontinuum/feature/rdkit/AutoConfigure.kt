2a7612513a93d35e5d9357ff5fe809c969900c6c
roushan65 <roushan65@gmail.com>
2026-03-15 18:26:30 +0530

    add ComponentScan

--- features/continuum-feature-rdkit/src/main/kotlin/org/projectcontinuum/feature/rdkit/AutoConfigure.kt
+++ features/continuum-feature-rdkit/src/main/kotlin/org/projectcontinuum/feature/rdkit/AutoConfigure.kt
@@ -1,6 +1,8 @@
 package org.projectcontinuum.feature.rdkit
 
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@ComponentScan
 class AutoConfigure
